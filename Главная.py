4dbe8e0810a5e84b7c5d985ac079740b12563666
Mikhail_Yelesin <elesin@gsz.su>
2026-01-22 17:50:28 +0300

    0005

--- Главная.py
+++ Главная.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Учусь Streamlit", layout="wide")
 
-st.title("🚀 Мой первый Streamlit-проект!!!!!")
+st.title("🚀 Мой первый Streamlit-проект!!!!!5")
 
 name = st.text_input("Как тебя зовут?")
 
